c2d890c94481e40129b26b48df6af50d4596db30
Aleksei Litivshko <uthhar@yandex.ru>
2020-04-09 00:13:12 +0700

    верстка баннеров

--- local/templates/germen/components/bitrix/catalog.section/banner/template.php
+++ local/templates/germen/components/bitrix/catalog.section/banner/template.php
@@ -10,13 +10,13 @@ $img = array('src' => '');
 if (!empty($item['PROPERTIES']['BANNER_IMG']['VALUE'])) {
     $img = CFile::ResizeImageGet(
         $item['PROPERTIES']['BANNER_IMG']['VALUE'],
-        array('width' => 1200, 'height' => 600),
+        array('width' => 1400, 'height' => 376),
         BX_RESIZE_IMAGE_PROPORTIONAL
     );
 } elseif (!empty($item['PREVIEW_PICTURE'])) {
     $img = CFile::ResizeImageGet(
         $item['PREVIEW_PICTURE'],
-        array('width' => 1200, 'height' => 600),
+        array('width' => 1400, 'height' => 376),
         BX_RESIZE_IMAGE_PROPORTIONAL
     );
 }
@@ -29,41 +29,52 @@ if (!empty($item['PROPERTIES']['BANNER_IMG']['VALUE'])) {
         <?php endif; ?>
     </div>
     <?php if (!empty($item)): ?>
-        <a href="#" class="js-detail" data-id="<?=$item['ID']?>">
-            <img src="<?=$img['src']?>" alt="<?=$item['NAME']?>">
-        </a>
-        <a href="#" class="promo-item__title js-detail" data-id="<?=$item['ID']?>">
-            <?php if (empty($item['PROPERTIES']['BANNER_TITLE']['VALUE'])): ?>
-                <?=$item['NAME']?>
-            <?php else: ?>
-                <?=$item['PROPERTIES']['BANNER_TITLE']['VALUE']?>
-            <?php endif; ?>
-        </a>
-        <?php if (empty($item['PROPERTIES']['BANNER_TEXT']['VALUE']['TEXT'])): ?>
-            <?=$item['PREVIEW_TEXT']?>
-        <?php else: ?>
-            <?=$item['PROPERTIES']['BANNER_TEXT']['VALUE']['TEXT']?>
-        <?php endif; ?>
-        <div class="promo-item__price">
-            <?=number_format($item['ITEM_PRICES'][0]['PRICE'], 0, '', ' ')?>
-            <span class="rouble"></span>
-        </div>
-        <a
-                href="/order/?id=<?=$item['ID']?>"
-                class="promo-item__delivery"
-                style="background-color: #<?=$item['BUTTON_PARAMS']['background']?>;"
-        >
-            <div class="promo-item__delivery__text"><?=$item['BUTTON_PARAMS']['text']?></div>
-            <div class="promo-item__delivery__time">
-                <?php if ($item['BUTTON_PARAMS']['showIcon']): ?>
-                    <svg class="promo-item__delivery__icon">
-                        <use xlink:href="<?=SITE_TEMPLATE_PATH?>/icons/icons.svg?v=<?=VERSION_SPRITE__ICONS?>#properties-car-mini"></use>
-                    </svg>
-                <?php endif; ?>
-                <div class="promo-item__delivery__time__text js-prod_time">
-                    <?=$item['BUTTON_PARAMS']['time']?>
-                </div>
+        <div class="promo-catalog__banner">
+            <a href="#" class="js-detail" data-id="<?=$item['ID']?>">
+                <picture>
+                    <source srcset="<?=$img['src']?>" media="(max-width: 768px)">
+                    <img src="<?=$img['src']?>" alt="<?=$item['NAME']?>">
+                </picture>
+            </a>
+            <div class="promo-catalog__banner-block">
+                <h3>
+                    <a href="#" class="js-detail" data-id="<?=$item['ID']?>">
+                        <?php if (empty($item['PROPERTIES']['BANNER_TITLE']['VALUE'])): ?>
+                            <?=$item['NAME']?>
+                        <?php else: ?>
+                            <?=$item['PROPERTIES']['BANNER_TITLE']['VALUE']?>
+                        <?php endif; ?>
+                    </a>
+                </h3>
+                <p>
+                    <?php if (empty($item['PROPERTIES']['BANNER_TEXT']['VALUE']['TEXT'])): ?>
+                        <?=$item['PREVIEW_TEXT']?>
+                    <?php else: ?>
+                        <?=$item['PROPERTIES']['BANNER_TEXT']['VALUE']['TEXT']?>
+                    <?php endif; ?>
+                </p>
+                <span class="promo-catalog__banner-price">
+                    <?=number_format($item['ITEM_PRICES'][0]['PRICE'], 0, '', ' ')?>
+                    <span class="rouble"></span>
+                </span>
+                <a
+                        href="/order/?id=<?=$item['ID']?>"
+                        class="promo-item__delivery"
+                        style="background-color: #<?=$item['BUTTON_PARAMS']['background']?>;"
+                >
+                    <div class="promo-item__delivery__text"><?=$item['BUTTON_PARAMS']['text']?></div>
+                    <div class="promo-item__delivery__time">
+                        <?php if ($item['BUTTON_PARAMS']['showIcon']): ?>
+                            <svg class="promo-item__delivery__icon">
+                                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/icons/icons.svg?v=<?=VERSION_SPRITE__ICONS?>#properties-car-mini"></use>
+                            </svg>
+                        <?php endif; ?>
+                        <div class="promo-item__delivery__time__text js-prod_time">
+                            <?=$item['BUTTON_PARAMS']['time']?>
+                        </div>
+                    </div>
+                </a>
             </div>
-        </a>
+        </div>
     <?php endif; ?>
 </div>
