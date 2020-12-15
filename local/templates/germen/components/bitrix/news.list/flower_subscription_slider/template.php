77e07152fa4e2803f4561f9ed38543646b7aafc2
Aleksei Litivshko <uthhar@yandex.ru>
2020-12-15 19:05:22 +0700

    новый слайдер подписки

--- local/templates/germen/components/bitrix/news.list/flower_subscription_slider/template.php
+++ local/templates/germen/components/bitrix/news.list/flower_subscription_slider/template.php
@@ -6,59 +6,77 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 
 $this->setFrameMode(true);
 ?>
-<?php if (!empty($arResult['ITEMS'])): ?>
-    <div class="swiper-wrapper">
-        <?php foreach ($arResult['ITEMS'] as $item): ?>
-            <?php
-            $src = '';
-            $srcset = '';
-            if (!empty($item['PREVIEW_PICTURE']) && !empty($item['DETAIL_PICTURE'])) {
-                $image = CFile::ResizeImageGet(
-                    $item['PREVIEW_PICTURE'],
-                    array('width' => 953, 'height' => 688),
-                    BX_RESIZE_IMAGE_PROPORTIONAL
-                );
+<div class="hero">
+    <div class="hero__text">
+        <h2 class="hero__title">
+            Подписка на цветы
+            <?php if (!empty($arResult['ITEMS'])): ?>
+                <span class="change-title" style="color:#FF323D">
+                    для
+                    <?php
+                    $first = true;
+                    ?>
+                    <?php foreach ($arResult['ITEMS'] as $item): ?>
+                        <span class="place <?=$item['CODE']?> <?=$first ? '' : 'hidden'?>"><?=$item['NAME']?></span>
+                        <?php
+                        $first = false;
+                        ?>
+                    <?php endforeach; ?>
+                </span>
+            <?php endif; ?>
+        </h2>
+        <div class="hero__intro">
+            Подписка — это еженедельная доставка цветов. Свежие авторские букеты у вас дома. Составные или монобукеты,
+            как вам больше нравится.
+        </div>
+        <a class="hero__btn button" href="/subscribe/choice/">Подписаться</a>
+    </div>
+    <div class="hero__slider">
+        <div class="swiper-container hero__slider-container-index">
+            <div class="swiper-wrapper">
+                <?php foreach ($arResult['ITEMS'] as $item): ?>
+                    <?php
+                    $src = '';
+                    $srcset = '';
+                    if (!empty($item['PREVIEW_PICTURE']) && !empty($item['DETAIL_PICTURE'])) {
+                        $image = CFile::ResizeImageGet(
+                            $item['PREVIEW_PICTURE'],
+                            array('width' => 953, 'height' => 688),
+                            BX_RESIZE_IMAGE_PROPORTIONAL
+                        );
 
-                $src = $image['src'];
-                $srcset = $item['DETAIL_PICTURE']['SRC'];
-            } elseif (!empty($item['PREVIEW_PICTURE'])) {
-                $image = CFile::ResizeImageGet(
-                    $item['PREVIEW_PICTURE'],
-                    array('width' => 953, 'height' => 688),
-                    BX_RESIZE_IMAGE_PROPORTIONAL
-                );
+                        $src = $image['src'];
+                        $srcset = $item['DETAIL_PICTURE']['SRC'];
+                    } elseif (!empty($item['PREVIEW_PICTURE'])) {
+                        $image = CFile::ResizeImageGet(
+                            $item['PREVIEW_PICTURE'],
+                            array('width' => 953, 'height' => 688),
+                            BX_RESIZE_IMAGE_PROPORTIONAL
+                        );
 
-                $src = $image['src'];
-                $srcset = $item['PREVIEW_PICTURE']['SRC'];
-            } elseif (!empty($item['DETAIL_PICTURE'])) {
-                $image = CFile::ResizeImageGet(
-                    $item['DETAIL_PICTURE'],
-                    array('width' => 953, 'height' => 688),
-                    BX_RESIZE_IMAGE_PROPORTIONAL
-                );
+                        $src = $image['src'];
+                        $srcset = $item['PREVIEW_PICTURE']['SRC'];
+                    } elseif (!empty($item['DETAIL_PICTURE'])) {
+                        $image = CFile::ResizeImageGet(
+                            $item['DETAIL_PICTURE'],
+                            array('width' => 953, 'height' => 688),
+                            BX_RESIZE_IMAGE_PROPORTIONAL
+                        );
 
-                $src = $image['src'];
-                $srcset = $item['DETAIL_PICTURE']['SRC'];
-            }
+                        $src = $image['src'];
+                        $srcset = $item['DETAIL_PICTURE']['SRC'];
+                    }
 
-            if (empty($src) && empty($srcset)) {
-                continue;
-            }
-            ?>
-            <div class="swiper-slide">
-                <img src="<?=$src?>" srcset="<?=$srcset?> 2x" alt="">
+                    if (empty($src) && empty($srcset)) {
+                        continue;
+                    }
+                    ?>
+                    <div class="swiper-slide swiper-slide-<?=$item['CODE']?>">
+                        <img src="<?=$src?>" srcset="<?=$srcset?> 2x" alt="">
+                    </div>
+                <?php endforeach; ?>
             </div>
-        <?php endforeach; ?>
+            <div class="hero__slider-pagination swiper-pagination"></div>
+        </div>
     </div>
-    <div class="hero__slider-pagination swiper-pagination"></div>
-    <div class="hero__slider-button-prev swiper-button-prev">
-        <svg width="11" height="18" aria-hidden="true">
-            <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/flower_subscription.svg#slider-arrow-prev"></use>
-        </svg>
-    </div>
-    <div class="hero__slider-button-next swiper-button-next">
-        <svg width="11" height="18" aria-hidden="true">
-            <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/flower_subscription.svg#slider-arrow-next"></use>
-        </svg>
-    </div>
-<?php endif; ?>
+</div>
