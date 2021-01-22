3146cf434c0b0f59f0fa7b8b2a9c4cc4d84da2cd
Ksenia Balashova <utyatka@gmail.com>
2021-01-22 14:42:01 +0100

    fix

--- local/templates/germen/components/bitrix/catalog.element/popup/template.php
+++ local/templates/germen/components/bitrix/catalog.element/popup/template.php
@@ -1,29 +1,73 @@
+<?php
+
+/**
+ * @var array $arResult
+ * @var array $arParams
+ */
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$label = '';
+$labelClass = '';
+if(!empty($arResult['PROPERTIES']['LABEL']['VALUE'])) {
+    $label = $arResult['PROPERTIES']['LABEL']['VALUE'];
+    if($arResult['PROPERTIES']['LABEL']['VALUE_XML_ID'] === 'season') {
+        $labelClass = 'product-info__season';
+    }
+}
+
+$wishlistClass = in_array($arResult['ID'], $arParams['WISHLIST'], true) ? 'red-heart' : '';
+$wishlistClassMobile = in_array($arResult['ID'], $arParams['WISHLIST'], true) ? 'white-heart' : '';
+
+$deliveryTime = $arResult['BUTTON_PARAMS']['time'];
+if(empty($deliveryTime)) {
+    $deliveryTime = $arResult['DELIVERY_TIME'];
+}
+
+$priceFormat = number_format($arResult['PRICES']['BASE']['VALUE'], 0, '', ' ');
+?>
 <div class="product-info__gallery">
-    <div class="product-slider swiper-container" aria-label="Close">
-        <div class="swiper-wrapper">
-            <?php foreach ($arResult["IMAGES"] as $arImage): ?>
+    <?php if (!empty($label)): ?>
+        <div class="<?=$labelClass?>"><?=$label?></div>
+    <?php endif; ?>
+    <div class="product-slider swiper-container">
+        <div class="swiper-wrapper" aria-label="Close" data-dismiss="modal">
+            <?php foreach ($arResult['IMAGES'] as $image): ?>
                 <div class="swiper-slide product-slider__item">
-                    <img src="<?=$arImage["SRC"]?>" alt="">
+                    <img src="<?=$image['SRC']?>" alt="">
                 </div>
             <?php endforeach; ?>
         </div>
+        <div class="swiper-button-prev product-slider__prev product-slider__btn">
+            <svg width="11" height="18" aria-hidden="true">
+                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#slider-arrow-prev"></use>
+            </svg>
+        </div>
+        <div class="swiper-button-next product-slider__next product-slider__btn">
+            <svg width="11" height="18" aria-hidden="true">
+                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#slider-arrow-next"></use>
+            </svg>
+        </div>
         <div class="swiper-pagination product-slider__pagination"></div>
     </div>
+    <button class="product-info__favorite-mob <?=$wishlistClassMobile?>" type="button" data-id="<?=$arResult['ID']?>">
 </div>
 <div class="product-info__content">
     <div class="product-info__content-inner">
         <div class="product-info__header">
             <h2 class="product-info__title">
-                <?=$arResult["NAME"]?>
+                <?=$arResult['NAME']?>
             </h2>
             <div class="product-info__discription">
-                <h3 class="product-info__discription-title">Описание</h3>
                 <div class="product-info__discription-text">
-                    <?=$arResult["PREVIEW_TEXT"]?>
+                    <?=$arResult['PREVIEW_TEXT']?>
                     <div class="product-info__discription-show"></div>
                 </div>
             </div>
             <form>
+                <?php /*
                 <div class="product-info__color">
                     <div class="product-info__color-inner">
                         <input class="visually-hidden" id="red" type="radio" name="color" value="red" checked>
@@ -34,13 +78,36 @@
                         <label for="green" style="background-color: #557464; border: 1px solid #557464;"></label>
                     </div>
                 </div>
+                */ ?>
                 <div class="product-info__cover">
                     <div class="product-info__cover-inner">
-                        <input class="visually-hidden" id="craft" type="radio" name="cover" value="craft" checked>
+                        <input
+                                class="visually-hidden"
+                                id="craft"
+                                type="radio"
+                                name="cover"
+                                value="craft"
+                                checked
+                                data-text="Упаковка: Стандартная крафтовая бумага"
+                        >
                         <label for="craft">Крафт</label>
-                        <input class="visually-hidden" id="film" type="radio" name="cover" value="film">
+                        <input
+                                class="visually-hidden"
+                                id="film"
+                                type="radio"
+                                name="cover"
+                                value="film"
+                                data-text="Упаковка: Стандартная пленка"
+                        >
                         <label for="film">Пленка</label>
-                        <input class="visually-hidden" id="nothing" type="radio" name="cover" value="nothing">
+                        <input
+                                class="visually-hidden"
+                                id="nothing"
+                                type="radio"
+                                name="cover"
+                                value="nothing"
+                                data-text="Упаковка: Без упаковки"
+                        >
                         <label for="nothing">Без упаковки</label>
                         <div class="product-info__chosen"></div>
                     </div>
@@ -48,74 +115,30 @@
                 </div>
             </form>
         </div>
-        <div class="product-info__add">
-            <h2 class="product-info__add-title">Добавить к заказу</h2>
-            <div class="product-info__add-slider swiper-container product-add-slider">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide product-add-slider__item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/sweets.jpg" alt="Конфеты">
-                        <div class="product-add-slider__header">
-                            <h3 class="product-add-slider__title">Конфеты AOKI</h3>
-                            <p class="product-add-slider__info"></p>
-                            Наш курьер зайдет за любимым вином или десертом. Стоимость товара оплачивается отдельно по
-                            чеку.
-                            <button class="product-add-slider__btn product-add-slider__btn--is-chosen">
-                                <svg width="20" height="20" aria-hidden="true">
-                                    <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#round-tick"></use>
-                                </svg>
-                            </button>
-                        </div>
+        <?php if (!empty($arResult['UPSALE_PRODUCTS'])): ?>
+            <div class="product-info__add">
+                <h2 class="product-info__add-title">Добавить к заказу</h2>
+                <div class="product-info__add-slider swiper-container product-add-slider">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($arResult['UPSALE_PRODUCTS'] as $item): ?>
+                            <div class="swiper-slide product-add-slider__item">
+                                <img src="<?=$item['image']?>" alt="<?=$item['name']?>">
+                                <div class="product-add-slider__header">
+                                    <h3 class="product-add-slider__title"><?=$item['name']?></h3>
+                                    <p class="product-add-slider__info"></p>
+                                    <?=$item['text']?>
+                                    <button class="product-add-slider__btn js-upsale-button" data-id="<?=$item['id']?>">
+                                        <svg width="20" height="20" aria-hidden="true">
+                                            <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#round-tick"></use>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
-                    <div class="swiper-slide product-add-slider__item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/sweets.jpg" alt="Конфеты">
-                        <div class="product-add-slider__header">
-                            <h3 class="product-add-slider__title">Конфеты AOKI</h3>
-                            <p class="product-add-slider__info"></p>
-                            Наш курьер зайдет за любимым вином или десертом. Стоимость товара оплачивается отдельно по
-                            чеку.
-                            <button class="product-add-slider__btn product-add-slider__btn--is-chosen">
-                                <svg width="20" height="20" aria-hidden="true">
-                                    <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#round-tick"></use>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="swiper-slide product-add-slider__item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/sweets.jpg" alt="Конфеты">
-                        <div class="product-add-slider__header">
-                            <h3 class="product-add-slider__title">Конфеты AOKI</h3>
-                            <p class="product-add-slider__info"></p>
-                            Наш курьер зайдет за любимым вином или десертом. Стоимость товара оплачивается отдельно по
-                            чеку.
-                            <button class="product-add-slider__btn product-add-slider__btn--is-chosen">
-                                <svg width="20" height="20" aria-hidden="true">
-                                    <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#round-tick"></use>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="swiper-slide product-add-slider__item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/sweets.jpg" alt="Конфеты">
-                        <div class="product-add-slider__header">
-                            <h3 class="product-add-slider__title">Конфеты AOKI</h3>
-                            <p class="product-add-slider__info"></p>
-                            Наш курьер зайдет за любимым вином или десертом. Стоимость товара оплачивается отдельно по
-                            чеку.
-                            <button class="product-add-slider__btn product-add-slider__btn--is-chosen">
-                                <svg width="20" height="20" aria-hidden="true">
-                                    <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#round-tick"></use>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-button-next product-add-slider__next">
-                    <svg width="11" height="18" aria-hidden="true">
-                        <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#slider-arrow-next-white"></use>
-                    </svg>
                 </div>
             </div>
-        </div>
+        <?php endif; ?>
         <div class="product-info__features">
             <div class="product-info__feature">
                 <div class="product-info__feature-icon">
@@ -175,7 +198,7 @@
                     <svg width="28" height="18" aria-hidden="true">
                         <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprites/sprite.svg#delivery-time"></use>
                     </svg>
-                    <span><?=$arResult["DELIVERY_TIME"]?></span>
+                    <span><?=$deliveryTime?></span>
                 </div>
                 <div class="product-info__cell">
                     <div class="payment-systems">
@@ -190,8 +213,14 @@
                             </svg>
                         </div>
                         <div class="payment-systems__item" aria-label="Visa">
-                            <svg class="payment-systems__icon payment-systems__icon--visa" width="49" height="17"
-                                 viewBox="0 0 49 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg
+                                    class="payment-systems__icon payment-systems__icon--visa"
+                                    width="49"
+                                    height="17"
+                                    viewBox="0 0 49 17"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                            >
                                 <path id="Vector" d="M18.6774 0.737724L12.288 15.9773H8.13382L4.99559 3.8308C4.81497 3.08575 4.63435 2.81483 4.06992 2.49875C3.0991 1.97947 1.54127 1.48277 0.164062 1.18927L0.254371 0.737724H6.95981C7.81774 0.737724 8.58537 1.30215 8.76598 2.29555L10.4367 11.1007L14.5458 0.737724H18.6774ZM35.0007 11.0104C35.0233 6.99162 29.4467 6.76584 29.4693 4.98224C29.4919 4.44039 30.0111 3.85338 31.14 3.69534C31.7044 3.62761 33.2623 3.55988 35.0233 4.37266L35.7232 1.14411C34.7749 0.805455 33.5558 0.466797 32.0431 0.466797C28.1598 0.466797 25.4054 2.5439 25.3828 5.50152C25.3602 7.69151 27.347 8.91068 28.8371 9.65573C30.3724 10.4008 30.8917 10.8749 30.8917 11.5522C30.8691 12.5682 29.6725 13.0197 28.5436 13.0423C26.5568 13.0649 25.4054 12.5005 24.5023 12.0715L23.7798 15.4129C24.7055 15.8419 26.3988 16.2031 28.1598 16.2257C32.2915 16.2257 34.9781 14.1712 35.0007 11.0104ZM45.2959 15.9773H48.9309L45.7475 0.737724H42.3835C41.6158 0.737724 40.9837 1.16669 40.7128 1.84401L34.7975 15.9548H38.9291L39.7419 13.6745H44.7992L45.2959 15.9773ZM40.8934 10.5814L42.9705 4.86936L44.1671 10.5814H40.8934ZM24.2991 0.737724L21.048 15.9773H17.1196L20.3707 0.737724H24.2991Z" fill="url(#paint0_linear)"/>
                                 <defs>
                                     <linearGradient id="paint0_linear" x1="0.164062" y1="8.34153" x2="48.9309" y2="8.34153" gradientUnits="userSpaceOnUse">
@@ -204,22 +233,30 @@
                     </div>
                 </div>
             </div>
-            <?php if (!$arResult["IS_ORDER"]): ?>
+            <?php if (!$arResult['IS_ORDER']): ?>
                 <div class="product-info__action">
                     <div class="product-info__ammount" id="input_div">
                         <input id="moins" type="button" value="-">
-                        <input id="count" type="number" size="1" value="1" max="99">
+                        <input id="count" type="number" size="1" value="1" max="99" name="product-quantity">
                         <input id="plus" type="button" value="+">
                     </div>
-                    <a class="button product-info__order-button" href="/order/?id=<?=$arResult["ID"]?>">
+                    <button
+                            class="button product-info__order-button js-product-order-button"
+                            href=""
+                            data-url="/order/"
+                            data-id="<?=$arResult['ID']?>"
+                    >
                         <div class="promo-item__delivery__text">
                             Заказать ·
-                            <span>
-                                <?=number_format($arResult["PRICES"]["BASE"]["VALUE"], 0, '', ' ')?>
-                            </span>
+                            <span><?=$priceFormat?></span>
                             ₽
                         </div>
-                    </a>
+                    </button>
+                    <button 
+                            class="product-info__favorite-desktop <?=$wishlistClass?>" 
+                            type="button" 
+                            data-id="<?=$arResult['ID']?>"
+                    ></button>
                 </div>
             <?php endif; ?>
         </div>
