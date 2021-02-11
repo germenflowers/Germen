c70a730d4237257a977b81d30c70aad4fc527d6a
Ksenia Balashova <utyatka@gmail.com>
2021-02-11 11:41:54 +0100

    add page

--- local/templates/germen/js/script.js
+++ local/templates/germen/js/script.js
@@ -9807,5 +9807,16 @@ window.productAddSlider = () => {
     elem: "tabs",
     open: 0
   });
+})();
 
+(function () {
+  var care = document.querySelector('.care__tabs');
+  if (!care) {
+    return;
+  }
+
+  var tabs = new Tabs({
+    elem: "care-tabs",
+    open: 0
+  });
 })();
