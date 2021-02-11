eea130fde63684c1e818756006501866ec51a68e
Aleksei Litivshko <uthhar@yandex.ru>
2021-02-11 18:36:25 +0700

    cart

---
+++ cart/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+
+$APPLICATION->SetTitle('Корзина');
+?>
+<?php
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
