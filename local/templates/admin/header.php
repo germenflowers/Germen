ef108123a7a05ca8689a4aa8ec97c7b0347c2571
Aleksei Litivshko <uthhar@yandex.ru>
2021-07-16 12:27:22 +0300

    pwa

--- local/templates/admin/header.php
+++ local/templates/admin/header.php
@@ -27,8 +27,20 @@ $userData = $user->getUserData();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#2F3BA2">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="apple-mobile-web-app-title" content="Germen Admin">
+        <meta name="application-name" content="Germen Admin">
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        <meta name="msapplication-config" content="/admin/browserconfig.xml">
 
         <title><?php $APPLICATION->ShowTitle() ?></title>
+
+        <link rel="manifest" href="/admin/manifest.json">
+        <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="<?=SITE_TEMPLATE_PATH?>/img/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?=SITE_TEMPLATE_PATH?>/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?=SITE_TEMPLATE_PATH?>/img/favicon-16x16.png">
 
         <?php $APPLICATION->ShowHead(); ?>
 
